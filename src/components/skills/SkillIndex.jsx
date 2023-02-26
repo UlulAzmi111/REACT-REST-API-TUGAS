191d1387298f4a73a9ee24cd05f0ceb70c6b99ff
UlulAzmi111 <azmiulul1453@gmail.com>
2023-02-27 01:52:29 +0700

    Menambahkan file untuk pembuatan rest api REACT JS dan menggunakan Framework CSS

---
+++ src/components/skills/SkillIndex.jsx
@@ -0,0 +1,62 @@
+import {  useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import SkillContext from "../../Context/SkillContext";
+
+export const SkillIndex = () => {
+  
+  const {skills, getSkills, deleteSkill} = useContext(SkillContext);
+
+  useEffect(() => {
+    getSkills();
+  }, []);
+
+  return (
+    <div className="mt-12">
+
+    <div className="flex justify-end m-2 p-2">
+      <Link to="/skills/create" className="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">New Skill</Link>
+    </div>
+
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Slug
+              </th>
+              <th scope="col" className="px-9 py-3">
+                Edit
+              </th>
+              <th scope="col" className="px-3 py-3">
+                Delete
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {skills.map((skill) => {
+              return (
+                <tr
+                  key={skill.id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <td className="px-6 py-4">{skill.name}</td>
+                  <td className="px-6 py-4">{skill.slug}</td>
+                  <td className="px-6 py-4">
+                    <Link to={`/skills/${skill.id}/edit`} className="px-4 py-2 bg-green-500 hover:bg-green-700 text-white rounded-md">Edit</Link>
+                  </td>
+                  <td>
+                    <button onClick={() => deleteSkill(skill.id)} className="px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded-md">Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
